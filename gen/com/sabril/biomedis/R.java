c87ebf28f46b969c45342730c5fbc10f3a39c0de
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2011-12-19 14:25:44 +0700

    fix socket

--- gen/com/sabril/biomedis/R.java
+++ gen/com/sabril/biomedis/R.java
@@ -21,16 +21,17 @@ public final class R {
         public static final int ic_tab_settings_unselected=0x7f020007;
     }
     public static final class id {
-        public static final int button1=0x7f060007;
-        public static final int ecg_graph=0x7f060002;
-        public static final int editText1=0x7f060004;
-        public static final int editText2=0x7f060006;
+        public static final int button1=0x7f060008;
+        public static final int ecg_graph=0x7f060003;
+        public static final int editText1=0x7f060005;
+        public static final int editText2=0x7f060007;
+        public static final int mHealthLabel=0x7f060002;
         public static final int mTimeLabel=0x7f060001;
-        public static final int ppg_graph=0x7f060003;
-        public static final int start=0x7f060008;
-        public static final int stop=0x7f060009;
+        public static final int ppg_graph=0x7f060004;
+        public static final int start=0x7f060009;
+        public static final int stop=0x7f06000a;
         public static final int textView1=0x7f060000;
-        public static final int textView2=0x7f060005;
+        public static final int textView2=0x7f060006;
     }
     public static final class layout {
         public static final int about=0x7f030000;
--- src/com/sabril/biomedis/GraphActivity.java
+++ src/com/sabril/biomedis/GraphActivity.java
@@ -1,15 +1,15 @@
 package com.sabril.biomedis;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LineGraphView;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-import com.jjoe64.graphview.GraphView.GraphViewSeries;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,55 +20,32 @@ import android.widget.Toast;
 
 public class GraphActivity extends Activity{
 	//private Timer mTimer;
-	private TextView timeLabel;
+	private TextView timeLabel, healthLabel;
 	private long mStartTime;
-	private Handler mHandler = new Handler();
-	private GraphViewData[] data; 
+	private Handler mHandler;
+	private Socket socket;
+	private LinearLayout ecg_section;
+	private Biomedis app;
+	
+	
+	private boolean connected = false;
+	
+	Thread fst;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.graphs);
-        //mStartTime = 0l;
-        
+        app = (Biomedis)getApplicationContext();
         timeLabel = (TextView)findViewById(R.id.mTimeLabel);
+        healthLabel = (TextView)findViewById(R.id.mHealthLabel);
         
-        LinearLayout ecg_section = (LinearLayout)findViewById(R.id.ecg_graph);
-        LinearLayout ppg_section = (LinearLayout)findViewById(R.id.ppg_graph);
+        ecg_section = (LinearLayout)findViewById(R.id.ecg_graph);
         
-        GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
-                new GraphViewData(1, 2.0d)
-                , new GraphViewData(2, 1.5d)
-                , new GraphViewData(3, 2.5d)
-                , new GraphViewData(4, 1.0d)
-        });
+        fst = new Thread(new ClientThread());
         
-        GraphView ecg_view = new LineGraphView(
-        	      this // context
-        	      , "ECG" // heading
-        	);
-        GraphView ppg_view = new LineGraphView(
-      	      this // context
-      	      , "PPG" // heading
-      	);
-        
-		// draw random curve
-        int num = 1000;
-        data = new GraphViewData[num];
-		double v=0;
-		for (int i=0; i<num; i++) {
-			v += 0.2;
-			data[i] = new GraphViewData(i, Math.sin(Math.random()*v));
-		}
-        
-        ecg_view.addSeries(new GraphViewSeries("", Color.rgb(90,250,0),data)); // data
-        ecg_view.setViewPort(2, 50);
-        ecg_view.setScrollable(true);
-        ecg_view.setScalable(true);
-        ppg_view.addSeries(exampleSeries); // data
-        
-        ecg_section.addView(ecg_view);
-        ppg_section.addView(ppg_view);
-        
+        mHandler = new Handler();
     }
     
     @Override
@@ -84,15 +61,17 @@ public class GraphActivity extends Activity{
         switch (item.getItemId()) {
         case R.id.start:
         	Toast.makeText(this, "Starting..", Toast.LENGTH_LONG).show();
-        	//if  (mStartTime == 0L){
-        		mStartTime = System.currentTimeMillis();
-        	//}
-        	mHandler.removeCallbacks(Timer_Tick);
+    		mStartTime = System.currentTimeMillis();
+    		mHandler.removeCallbacks(Timer_Tick);
     		mHandler.postDelayed(Timer_Tick, 100);
+    		//mHandler.post(Listen_Server);
+        	fst.start();
             return true;
         case R.id.stop:
         	Toast.makeText(this, "Stopping..", Toast.LENGTH_LONG).show();
         	mHandler.removeCallbacks(Timer_Tick);
+        	//mHandler.removeCallbacks(Listen_Server);
+        	fst.stop();
             return true;
         default:
             return super.onOptionsItemSelected(item);
@@ -111,15 +90,77 @@ public class GraphActivity extends Activity{
 		    } else {
 		    	timeLabel.setText("" + minutes + ":" + seconds);            
 		    }
+		    mHandler.postDelayed(this, 1000);
 		    
-		    
-		    
-		    
-		    mHandler.postDelayed(this, 200);
 		}
 	};
 	
-	public boolean resetGraph(){
-		return true;
+	private void updateLabel(String time_string){
+		healthLabel.setText(time_string);
+	}
+	
+	private Runnable Listen_Server = new Runnable() {
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+            try {
+                InetAddress serverAddr = InetAddress.getByName(app.getIpAddress());
+                Log.d("ClientActivity", "C: Connecting...");
+                socket = new Socket(serverAddr, Integer.parseInt(app.getPortNumber()));
+                connected = true;
+                while (connected) {
+                    try {
+                        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                        out.flush();
+                        String output = dataInputStream.readLine();
+                        String[] temp = output.split(" ");
+                        Log.d("ClientActivity", "" +temp[2]);
+                        //updateLabel(temp[2]);
+                    } catch (Exception e) {
+                        Log.e("ClientActivity", "S: Error", e);
+                    }
+                }
+                socket.close();
+                Log.d("ClientActivity", "C: Closed.");
+            } catch (Exception e) {
+                Log.e("ClientActivity", "C: Error", e);
+                connected = false;
+            }
+		}
+	};
+	
+	public class ClientThread implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+            try {
+                InetAddress serverAddr = InetAddress.getByName(app.getIpAddress());
+                Log.d("ClientActivity", "C: Connecting...");
+                socket = new Socket(serverAddr, Integer.parseInt(app.getPortNumber()));
+                connected = true;
+                while (connected) {
+                    try {
+                        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                        out.flush();
+                        String output = dataInputStream.readLine();
+                        String[] temp = output.split(" ");
+                        Log.d("ClientActivity", "" +temp[2]);
+                        //updateLabel(temp[2]);
+                    } catch (Exception e) {
+                        Log.e("ClientActivity", "S: Error", e);
+                    }
+                }
+                socket.close();
+                Log.d("ClientActivity", "C: Closed.");
+            } catch (Exception e) {
+                Log.e("ClientActivity", "C: Error", e);
+                connected = false;
+            }
+		}
+		
 	}
 }
